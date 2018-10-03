c1d8c4c7f0786c0519626d325fcb5d5c833d5315
IYAD SHAHEEN <eddie@IYADs-MacBook-Pro.local>
2018-10-03 11:36:32 -0700

    removed notification button from within the notification page

--- src/views/Notifications.vue
+++ src/views/Notifications.vue
@@ -2,19 +2,16 @@
   <div class="notifications">
     <MenuBar/>
     <h1>Notifications</h1>
-    <NotificationButton/>
   </div>
 </template>
 
 <script>
 import MenuBar from '@/components/MenuBar'
-import NotificationButton from '@/components/NotificationButton'
 
 export default {
   name: 'Notifications',
   components: {
     MenuBar,
-    NotificationButton
   }
 }
 </script>

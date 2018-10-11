93b7382a55529ad57abbf1cfd7b35201cc7161c1
Christian Martinez <christian.martinez442@yahoo.com>
2018-10-11 11:15:43 -0700

    added a .png file that goes into the home page. Also im trying to get the overall look of the home page at least outined. Still working on how things work in html and CSS.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="home">
     <MenuBar/>
-    <h1>Home</h1>
+
+    <!--Trade shift inputs -->
+    <form name="shiftTradeForm"> 
+      
+      <div class ="tradePanel"> 
+        <div class="yourShiftTextArea">
+            <input type="text" name="yourShift" placeholder="Your Shift...">
+        </div>
+        <div class="theirShiftBox">
+          <div class="theirShiftText">
+            <input type="text" name="theirShift" placeholder="Their Shift...">
+          </div>
+        </div>
+        <div class="switchImage">
+          <img src="@/assets/Switch.png" class="switchIcon">
+        </div>
+
+        <div class="">
+          <input type="submit" Value="TRADE!" class="tradeButton">
+        </div>
+        
+      </div>
+
+    </form>
     <NotificationButton/>
   </div>
 </template>
@@ -18,3 +41,97 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  
+  .tradeButton{
+
+    position: absolute;
+    width: 104.44px;
+    height: 31.83px;
+    left: 501.41px;
+    top: 163.21px;
+
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    line-height: normal;
+    font-size: 18px;
+
+    color: #000000;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 4px 2px rgba(0, 0, 0, 0.1);
+    background: #59B9FF;
+    border-radius: 15px;
+}
+   
+.tradePanel{ 
+ 
+  position: absolute;
+  width: 871px;
+  height: 191px;
+  left: 115px;
+  top: 38px;
+  background: #BBDFF9;
+  border-radius: 15px;
+
+}
+.switchImage{
+  position: absolute;
+  width: 52.22px;
+  height: 53.06px;
+  left: 527.52px;
+  top: 81.51px;
+}
+
+.theirShiftBox{ 
+  position: absolute;
+  width: 300px;
+  height: 30px;
+  left: 534px;
+  top: 79px;
+  background: #FFFFFF;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.theirShiftText{ 
+
+  position: absolute;
+  width: 300px;
+  height: 30px;
+  left: 300px;
+  top: 104px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: normal;
+  font-size: 18px;
+  text-align: center;
+
+  color: rgba(0, 0, 0, 0.5);
+}
+
+
+.yourShiftTextArea{  
+
+  position: absolute;
+  width: 300px;
+  height: 30px;
+  left: 184px;
+  top: 99px;
+  background: #FFFFFF;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: normal;
+  font-size: 21px;
+  text-align: center;
+  display: inline-block;
+  color: rgba(0, 0, 0, 0.5);
+}
+
+
+
+
+</style>

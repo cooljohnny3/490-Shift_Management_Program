43003bd3c16d7655ec8b9f3157f1a883c99f1f0e
John Brehm <cooljohnny3@gmail.com>
2018-10-11 11:17:35 -0700

    More login work

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,19 @@
 <template>
   <div class="login">
-		<div class="box1">
-			<h1>
-				SKEJ
-			</h1>
-				<div class="input">
-					<input v-model="username" type="text" placeholder = "Username"><br>
-					<input v-model="password" type="Password" placeholder = "Password">
-				</div>
-        <div class="buttons">
-          <router-link to="/">
-            <button>Log in</button>
-          </router-link>
-          <router-link to="/forgot">
-            <button>Forgot Password</button>
-          </router-link>
-        </div>
+      <div class="box1">
+        <h1>
+          SKEJ
+        </h1>
+          <div class="input">
+            <input v-model="email" type="text" placeholder = "Email" required autofocus><br>
+            <input v-model="password" type="Password" placeholder = "Password" required>
+          </div>
+          <div class="buttons">
+            <button type="submit" v-on:click="handleSubmit">Log in</button>
+            <router-link to="/forgot">
+              <button>Forgot Password</button>
+            </router-link>
+          </div>
 		</div>
 	</div>
 </template>
@@ -26,8 +24,43 @@ export default {
 
   data() {
     return {
-      username: '',
+      email: '',
       password: ''
+    }
+  },
+
+  methods: {
+    handleClick(e) {
+      e.preventDefault();
+      if (this.password.length > 0) {
+        this.$http.post('http://localhost:3000/login', {
+          email: this.email,
+          password: this.password
+        })
+        .then(response => {
+          let is_admin = response.data.user.is_admin
+          localStorage.setItem('user', JSON.stringify(response.data.user));
+          localStorage.setItem('jwt', response.data.token);
+
+          if (localStorage.getItem('jwt') != null){
+              this.$emit('loggedIn')
+              if(this.$route.params.nextUrl != null){
+                  this.$router.push(this.$route.params.nextUrl);
+              }
+              else {
+                  if(is_admin== 1){
+                      this.$router.push('admin');
+                  }
+                  else {
+                      this.$router.push('dashboard');
+                  }
+              }
+          }
+        })
+        .catch(function (error) {
+            console.error(error.response);
+        });
+      }
     }
   }
 }

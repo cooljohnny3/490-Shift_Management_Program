9d7422b162531bb2de2d410e53b9cc096f71ca7b
John Brehm <cooljohnny3@gmail.com>
2018-10-16 15:16:24 -0700

    Update database file

--- src/server/initilizeDB.sql
+++ src/server/initilizeDB.sql
@@ -22,14 +22,15 @@ CREATE TABLE IF NOT EXISTS marketplace (
               start_time TIME,
               end_time TIME,
               PRIMARY KEY (id));
-/*
+
 CREATE TABLE IF NOT EXISTS schedule (
               id INT AUTO_INCREMENT, 
-              name TINYTEXT,
-              email VARCHAR(50) UNIQUE, 
-              user_pass TINYTEXT,
-              is_admin INT,
+              first_name TINYTEXT,
+              last_name TINYTEXT,
+              date DATE,
+              start_time TIME,
+              end_time TIME,
               PRIMARY KEY (id));
-*/
+
 
 INSERT INTO users (first_name,email,user_pass,is_admin) VALUES ('admin', 'admin', '$2a$08$gaARy0Azr3lgf0BB6YkQ1ORobCasZIwBXQebAd2C8KedSyBJ6vZPC', 1);

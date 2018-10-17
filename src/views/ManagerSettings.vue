a8762e730a2ce98984f205feb6c059cb7d642138
Froilan Campos <froilan.campos.926@my.csun.edu>
2018-10-17 10:55:17 -0700

    v2. added create delete ,change password.(missing toggles)

--- src/views/ManagerSettings.vue
+++ src/views/ManagerSettings.vue
@@ -16,7 +16,7 @@
     
 
 <!-- Form Name -->
-<legend>Employee Details</legend>
+<legend>Employee Details:</legend>
 
 <!-- Text input-->
 <div class="form-group">
@@ -66,7 +66,7 @@
 </div>
 <br/>
 
-<legend>Hours Settings</legend>
+<legend>Hours Settings:</legend>
    
    <div class="form-group">
     <label for="regno" class="col-md-4 control-label">Max Hours per day</label>
@@ -162,6 +162,166 @@
 </fieldset>
 </form>
 
+<legend>Create new Employee:</legend>
+<br>
+<br>
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-1 control-label" for="displayname">Name:</label>  
+  <div class="col-md-4">
+  <input id="displayname" name="displayname" type="text" placeholder="Please enter an employee name " class="form-control input-md" required="">
+  <span class="help-block"></span>  
+  </div>
+</div>
+<br>
+<br>
+<!-- Prepended text-->
+<div class="form-group">
+  <label class="col-md-1 control-label" for="designation">Designation</label>
+    <div class="col-md-4">
+        <select id="designation" name="designation" class="form-control">
+            <option value="Director">Director</option>
+            <option value="ProductHead">Product Head</option>
+            <option value="GeneralManager">General Manager</option>
+            <option value="ProductManager">Product Manager</option>
+            <option value="TeamLead">Team Lead</option>
+        </select>
+    </div>
+</div>
+<br>
+<br>
+<div class="form-group">
+  <label class="col-md-1 control-label" for="department">Department</label>
+  <div class="col-md-4">
+    <select id="department" name="department" class="form-control">
+      <option value="Accounts">Accounts</option>
+      <option value="HR">HR</option>
+      <option value="Finance">Finance</option>
+      <option value="Loan">Loan</option>
+    </select>
+  </div>
+</div>
+<br>
+<br>
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-1 control-label" for="displayname">Employee Key:</label>  
+  <div class="col-md-4">
+  <input id="displayname" name="displayname" type="text" placeholder="Enter ID: " class="form-control input-md" required="">
+  <span class="help-block"></span>  
+  </div>
+</div>
+<br>
+<br>
+<div class="form-group">
+  <label class="col-md-1 control-label" for="displayname">Email</label>  
+  <div class="col-md-4">
+  <input id="displayname" name="displayname" type="text" placeholder="you@email.com " class="form-control input-md" required="">
+  <span class="help-block"></span>  
+  </div>
+</div>
+<br>
+<br>
+<div class="form-group">
+  <label class="col-md-1 control-label" for="displayname">Confirm Email:</label>  
+  <div class="col-md-4">
+  <input id="displayname" name="displayname" type="text" placeholder="Re-enter Email adress " class="form-control input-md" required="">
+  <span class="help-block"></span>  
+  </div>
+</div>
+<br>
+<br>
+<div class="form-group">
+  <label class="col-md-1 control-label" for="displayname">Password::</label>  
+  <div class="col-md-4">
+  <input id="displayname" name="displayname" type="text" placeholder="set new password " class="form-control input-md" required="">
+  <span class="help-block"></span>  
+  </div>
+</div>
+<br>
+<br>
+<div class="form-group">
+  <label class="col-md-1 control-label" for="displayname">Confirm Password:</label>  
+  <div class="col-md-4">
+  <input id="displayname" name="displayname" type="text" placeholder="Re-Enter Password " class="form-control input-md" required="">
+  <span class="help-block"></span>  
+  </div>
+</div>
+
+
+<br>
+<br>
+<br>
+<br>
+
+<legend>Password Settings:</legend>
+
+		<div class="col-sm-4">
+		    
+		    <label>Current Password</label>
+		    <div class="form-group pass_show"> 
+                <input type="password" value="John.doe@mail.com" class="form-control" placeholder="Current Password"> 
+            </div> 
+		       <label>New Password</label>
+            <div class="form-group pass_show"> 
+                <input type="password" value="John.doe@mail.com" class="form-control" placeholder="New Password"> 
+            </div> 
+		       <label>Confirm Password</label>
+            <div class="form-group pass_show"> 
+                <input type="password" value="John.doe@mail.com" class="form-control" placeholder="Confirm Password"> 
+            </div> 
+            
+		</div>  
+ 
+<br>
+<br>
+<br>
+<br>
+<legend>Delete Employee Account:</legend>
+<br>
+<br>
+<div class="form-group">
+  <label class="col-md-1 control-label" for="displayname">Name:</label>  
+  <div class="col-md-4">
+  <input id="displayname" name="displayname" type="text" placeholder="Please enter an employee name " class="form-control input-md" required="">
+  <span class="help-block"></span>  
+  </div>
+</div>
+<br>
+<br>
+<div class="form-group">
+  <label class="col-md-1 control-label" for="displayname">Email:</label>  
+  <div class="col-md-4">
+  <input id="displayname" name="displayname" type="text" placeholder="Re-enter Email adress " class="form-control input-md" required="">
+  <span class="help-block"></span>  
+  </div>
+</div>
+<br>
+<br>
+<div class="form-group">
+  <label class="col-md-1 control-label" for="displayname">Employee Key:</label>  
+  <div class="col-md-4">
+  <input id="displayname" name="displayname" type="text" placeholder="Enter ID: " class="form-control input-md" required="">
+  <span class="help-block"></span>  
+  </div>
+</div>
+<br>
+<br>
+<div class="form-group">
+  <label class="col-md-1 control-label" for="displayname">Administrator Password:</label>  
+  <div class="col-md-4">
+  <input id="displayname" name="displayname" type="text" placeholder="Enter Password " class="form-control input-md" required="">
+  <span class="help-block"></span>  
+  </div>
+</div>
+<br>
+<br>
+<br>
+<form action="" method="">
+    <input type="submit" name="submitbutton"value="Delete">
+    </form>
+
+<br/>
 </div>
   </div>
 </main>
